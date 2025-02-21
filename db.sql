7bbfc9d371fbb6ff8d673afad7b3b1913904ccff
augustinhocarraraa <fornepereiraj@gmail.com>
2025-02-21 10:12:33 -0300

    afazeres

---
+++ db.sql
@@ -0,0 +1,24 @@
+--CRIAÇÃO do Banco Inicial 
+CREATE DOLCEPIE;
+SELECT DOLCEPIE;
+
+--Criar a tabela  de usuario(Maria)
+
+
+--criar a tabela de produtos + tabela favorita (fernanda)
+
+
+--Criar a tabela de pedidos + Itens do pedido(Luiz)
+
+
+
+--Criar a tabela de carrinhos+ pagamentos Luna )
+
+
+
+
+--Criar a tabela de Estoque (Jeferson)
+
+
+
+
