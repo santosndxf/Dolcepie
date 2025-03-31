00d520b0a69da81115fd6cebd958113dcc8da0c0
Santosndxf <fernanda2007matos@gmail.com>
2025-03-31 09:41:04 -0300

    Update population.sql

--- population.sql
+++ population.sql
@@ -1,3 +1,1 @@
---populaçao da tabela de usuario)s  
-INSERT INTO cart(quantidade,preço_unitario,total,criado_em,atualizado_em)VALUE('6','R$2,00','R$12,00','22/02/2025','28/02/2025')
-INSERT INTO pay(valor_total,metodo_paagmento,status_pagamento,codigo_transaçao) VALUE('12,00','debito','aprovado','32456')
+
