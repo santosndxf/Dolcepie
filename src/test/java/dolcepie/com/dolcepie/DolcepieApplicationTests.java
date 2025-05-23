608e9268105ac24b012afe7b42f533013038d544
vitinho0005 <vitinho.vgzin@gmail.com>
2025-05-23 10:10:48 -0300

    update

---
+++ src/test/java/dolcepie/com/dolcepie/DolcepieApplicationTests.java
@@ -0,0 +1,13 @@
+package dolcepie.com.dolcepie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DolcepieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
