dc28f9dae3ec17b1613246ee6661e36453e840b8
marialuiza267 <marialuizabatista2612@gmail.com>
2025-05-23 15:46:14 -0400

    update

---
+++ src/main/entity/Favrito.java
@@ -0,0 +1,26 @@
+import jakarta.persistence.*;
+import java.util.*;
+
+@Entity
+@Table(name = "favoritos")
+public class Favorito {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    private Usuario usuario;
+
+    @ManyToOne
+    private Produto produto;
+
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+
+    public Usuario getUsuario() { return usuario; }
+    public void setUsuario(Usuario usuario) { this.usuario = usuario; }
+
+    public Produto getProduto() { return produto; }
+    public void setProduto(Produto produto) { this.produto = produto; }
+}
